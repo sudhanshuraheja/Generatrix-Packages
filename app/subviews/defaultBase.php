302a845521f87f7e4b55e109008058f6403d8a8e
Sudhanshu Raheja <sudhanshu.raheja@vxtindia.com>
2010-06-22 16:59:08 +0530

    Added packages

--- app/subviews/defaultBase.php
+++ app/subviews/defaultBase.php
@@ -5,7 +5,7 @@
 	<div class='span-24'>
 		<div class='span-6'>&nbsp;</div>
 		<div class='span-12'>
-			<a href='http://vxtindia.com/generatrixFramework'><img src='<?php echo href('/public/images/generatrix.png') ?>' alt='Generatrix! PHP, Javascript, CSS and the whole shebang#' /></a>
+			<a target='_blank' href='http://vxtindia.com/generatrixFramework'><img src='<?php echo href('/public/images/generatrix.png') ?>' alt='Generatrix! PHP, Javascript, CSS and the whole shebang#' /></a>
 		</div>
 		<div class='span-6 last'>&nbsp;</div>
 	</div>

4fd8a19a75e71a49cb82e936717a4df5879b2584
Sudhanshu Raheja <sudhanshu.raheja@vxtindia.com>
2010-07-02 17:37:34 +0530

    Added database for repo and releases

---
+++ app/model/databases.php
@@ -0,0 +1,28 @@
+<?php
+
+  class packages extends Model {
+    public function __construct($database) {
+      $this->construct($database, "packages", array(
+				"id" => "int",
+				"user" => "varchar_128",
+				"repo" => "varchar_128",
+				"is_approved" => "int",
+				"created" => "int",
+				"updated" => "int"
+      ));
+    }
+  }
+
+  class releases extends Model {
+    public function __construct($database) {
+      $this->construct($database, "releases", array(
+				"id" => "int",
+				"package" => "int",
+				"tag" => "varchar_128",
+				"created" => "int",
+				"is_deleted" => "int"
+      ));
+    }
+  }
+
+?>
